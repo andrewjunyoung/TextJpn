f14b74b8389fdad0f0eb7f7fed23c3a05e647562
andrewjunyoung <andrew.young16@imperial.ac.uk>
2017-12-21 15:41:19 +0000

    kana.hs: Moved type definitions here, and imported the Hiragana, Katakana modules

---
+++ kana.hs
@@ -0,0 +1,14 @@
+import Hiragana
+import Katakana
+
+type Kana = Char -- Hiragana | Katakana
+
+type Hiragana = Char
+type Katakana = Char
+type Kanji = Char
+
+type JpanChar = Char -- Kana | Kanji
+type JpanText = [JpanChar]
+
+type LatnChar = Char
+type LatnText = [LatnChar]
