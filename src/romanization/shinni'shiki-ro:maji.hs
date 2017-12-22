c8259f79392676e151c9f72413cd9af90f4d2b2c
andrewjunyoung <andrew.young16@imperial.ac.uk>
2017-12-22 01:05:46 +0000

    Setup cabal package

---
+++ src/romanization/shinni'shiki-ro:maji.hs
@@ -0,0 +1,32 @@
+module Shinni'shikiRomaji where
+
+import qualified Kana
+import qualified Hiragana as Hira
+import qualified Katakana as Kata
+
+romanize :: JapnString -> LatnString
+
+japanize :: LatnString -> JapnString
+
+romanizeKana :: Kana -> LatnString
+romanizeKana = liftM2 (++) initial final
+  where
+    initial :: Kana -> LatnString
+    initial c
+      | otherwise = ""
+    final :: Kana -> LatnString
+    final c
+      | elem c aColumn = "a" ++ modifier c
+      | elem c iColumn = "i" ++ modifier c
+      | elem c uColumn = "u" ++ modifier c
+      | elem c eColumn = "e" ++ modifier c
+      | elem c oColumn = "o" ++ modifier c
+      | c == 'ん'           = "'"
+      | otherwise = ""
+    modifier :: Kana -> LatnString
+    modifier = fromMaybe "" $ modifier'
+      where
+        modifier' :: Kana -> (Maybe LatnString)
+        modifier' c
+          | elem c small = Just " ́"
+          | otherwise         = Nothing
